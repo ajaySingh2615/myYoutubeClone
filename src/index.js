b56f9fe7c31569c8949d08c0d0cc14c5cef88c52
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-16 15:06:07 +0530

    "utils code discuss"

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running on port " + process.env.PORT);
+    });
+    app.on("error", (err) => {
+      console.error(err);
+      throw err;
+    });
+  })
+  .catch((error) => {
+    console.log(`MongoDB connection error: ${error}`);
+  });
